4285b94a10569ba8cafa100716718b7adede47c7
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-04 21:58:14 -0600

    add comments

--- review.py
+++ review.py
@@ -1,7 +1,9 @@
 # Printing my name
 
-print("Miguel Coria")
+# print("Miguel Coria")
 
-user_name = input("what is your name? \n")
+# Using input to get user's name
 
-print(f"Hello {user_name}!!")
+# user_name = input("what is your name? \n")
+
+# print(f"Hello {user_name}!!")
