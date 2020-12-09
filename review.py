87088bae379dc54bb7b7190b062f2bc252df94fc
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-09 17:22:22 -0600

    itrables

--- review.py
+++ review.py
@@ -124,5 +124,10 @@
 
 # print(can_message)
 
-for i in "miky":
-    print(i)
+# for i in "miky":
+#     print(i)
+
+for item in (1, 2, 3, 4, 5):
+    for x in ['a', 'b', 'c']:
+        print(item, x)
+
