b76915d5dd5a285857d16e6160996e3a9db2c4c9
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-09 20:02:18 -0600

    code refactor

--- review.py
+++ review.py
@@ -180,10 +180,12 @@ picture = [
     [0, 0, 0, 1, 0, 0, 0]
 ]
 
+fill = "*"
+empty= ''
 for row in picture:
     for pixel in row:
-        if (pixel == 1):
-            print("*", end="")
+        if pixel:
+            print(fill, end="")
         else:
-            print(" ", end="")
+            print(empty, end="")
     print("")
