4565676d407b63eb7433fc4e9e1dad303eda392e
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-10 22:56:42 -0600

    creating more

--- review.py
+++ review.py
@@ -249,12 +249,30 @@
 # print(player1.speak())
 
 class User:
+    def __init__(self, email):
+        self.email = email
     def sign_in(self):
         print('logged in')
 
 class Wizard(User):
-    pass
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'attacking with power of {self.power}')
+
 
 class Archer(User):
-    pass
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
 
+    def attack(self):
+        print(f'attacking with arrows: arrows left: {self.num_arrows}')
+        self.num_arrows = self.num_arrows - 1
+
+wizard1 = Wizard("miky", 50)
+archer1 = Archer("Robin", 100)
+
+print(archer1.attack())
