a071c390354dbd43fdbd89e23d8e436dc8d8af3c
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-13 21:05:59 -0600

    creating better errors

--- review.py
+++ review.py
@@ -371,7 +371,7 @@ def sum(num1, num2):
     try:
         return num1 + num2
     except TypeError as err:
-        print("Please enter numbers " + err)
+        print(f"Please enter numbers + {err}")
 
 print(sum('1', 2))
 
