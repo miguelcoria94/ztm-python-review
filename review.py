7ce2b448524e4f6ab92e22b161fb8659406265b6
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-14 21:14:28 -0600

    starting

--- review.py
+++ review.py
@@ -377,17 +377,19 @@
 
 #generators
 
-print(range(100))
-print(list(range(100)))
+# print(range(100))
+# print(list(range(100)))
 
-def make_list(num):
-    result = []
-    for i in range(num):
-        result.append(i * 2)
-    return result
+# def make_list(num):
+#     result = []
+#     for i in range(num):
+#         result.append(i * 2)
+#     return result
 
-my_list = make_list(100)
-print(my_list)
+# my_list = make_list(100)
+# print(my_list)
+
+
 
 
 
