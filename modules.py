09e7c2f39def8e61ce747106fbcf5113ae5b332c
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-15 23:53:07 -0600

    created a random num gen

--- modules.py
+++ modules.py
@@ -5,14 +5,17 @@ num_to_guess = random.randint(1, 10)
 
 
 while True:
-    guess = input('Guess a number 1-10 \n')
-    if int(guess) < 0:
-        print('Your number is to low')
-    elif int(guess) > 10:
-        print('Your number is to high')
-    elif int(guess) == num_to_guess:
-        print("You guessed the right number !!")
-        break
-    else:
-        print("Try Again")
+    try:
+        guess = input('Guess a number 1-10 \n')
+        if int(guess) < 0:
+            print('Your number is to low')
+        elif int(guess) > 10:
+            print('Your number is to high')
+        elif int(guess) == num_to_guess:
+            print("You guessed the right number !!")
+            break
+        else:
+            print("Try Again")
+    except ValueError:
+        print("I said guess a number")
     
