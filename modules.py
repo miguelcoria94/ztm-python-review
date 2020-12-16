59223db9f5b25ed9e32b43134b5abbf414f6e0f8
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-15 23:50:15 -0600

    creating a guessing game

--- modules.py
+++ modules.py
@@ -1,1 +1,17 @@
-import random
+import sys
+import random 
+
+num_to_guess = random.randint(1, 10)
+
+
+while True:
+    guess = input('Guess a number 1-10')
+    if int(guess) < 0:
+        print('Your number is to low')
+    elif int(guess) > 10:
+        print('Your number is to high')
+    elif int(guess) == num_to_guess:
+        print("You guessed the right number !!")
+    else:
+        print("Try Again")
+    
